73e3fcf9c60ae669d2d2ba8d3582fa87279c80c2
Shivansh Sharma <shivanshsharma8@gmail.com>
2023-04-23 01:16:40 -0400

    Chkpt 3: Users CRUD operations

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
     console.log('Server is running on port 8800');
